e97e160f18583daad90451cb86c075e60991ebd4
Munir Ahmad <munir.ehmad@gmail.com>
2011-07-01 00:03:56 +0500

    Removed application folder, added model

---
+++ models/transaction.php
@@ -0,0 +1,37 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Users
+ *
+ * This model represents user authentication data. It operates the following tables:
+ * - user account data,
+ * - user profiles
+ *
+ * @package	Tank_auth
+ * @author	Ilya Konyukhov (http://konyukhov.com/soft/)
+ */
+class Transaction extends CI_Model
+{
+	private $table_name			= 'transactions';			// user accounts
+
+	function __construct()
+	{
+		parent::__construct();
+
+		$ci =& get_instance();
+	}
+
+	function create($data)
+	{
+		if ($this->db->insert($this->table_name, $data))
+		 {
+			$transaction_id = $this->db->insert_id();
+			return array('transaction_id' => $transaction_id);
+		}
+		return NULL;
+	}
+
+}
+
+/* End of file users.php */
+/* Location: ./application/models/auth/users.php */
